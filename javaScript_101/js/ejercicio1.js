f8fc3d7df1f9b10002eeadab12afbedbe662edca
puribey <kenshipichu@gmail.com>
2017-09-27 15:37:27 -0300

    new ifs

--- javaScript_101/js/ejercicio1.js
+++ javaScript_101/js/ejercicio1.js
@@ -4,6 +4,7 @@ var monto=parseInt(ingrese); //convierte un string ingresado a numero
 var desc=0;
 var recargo=0;
 
+
 if (modo=="efectivo"){
 	if (monto>=500 && monto<1000) {
 		desc=(monto*5)/100;
@@ -20,6 +21,9 @@ if (modo=="efectivo"){
 		total=(monto-desc);
 		alert("Su monto a pagar es $"+total+".-");
 	}
+	else if (monto<500){
+ 		alert("Usted no tiene ningún descuento o recargo. Su monto a pagar es $"+monto+".-");
+ 	}
 
 } 
 else if (modo=="tarjeta") {
@@ -31,12 +35,15 @@ else if (modo=="tarjeta") {
  	else if (monto>=1000 && monto<2000) {
 		desc=(monto*10)/100;
 		recargo=(monto*2.5)/100;
-		total=(monto+recargo)+(monto-desc);
+		total=(monto+recargo)-desc;
 		alert("Su monto a pagar es $"+total+".-");
 	} 
  	else if (monto>=2000){
 		desc=(monto*10)/100;
 		total=(monto-desc);
 		alert("Su monto a pagar es $"+total+".-");
+ 	}
+ 	else if (monto<500){
+ 		alert("Usted no tiene ningún descuento o recargo. Su monto a pagar es $"+monto+".-");
  	}
 }
