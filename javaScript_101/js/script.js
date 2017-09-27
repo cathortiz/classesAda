ea8edae1b7283377b48cde7406d4592a00a39805
puribey <kenshipichu@gmail.com>
2017-09-27 14:40:57 -0300

    correccion

--- javaScript_101/js/script.js
+++ javaScript_101/js/script.js
@@ -3,9 +3,9 @@ var nombre, pass;
 
 
 function contador(form) {
-	if (form.name.value == "doctor"  form.pass.value == "1234" && cont<3){
+	if (form.name.value=="doctor" && form.pass.value=="1234" && cont<3){
 		alert("Bienvenido!");
-	} else if (form.namevalue != "doctor" || form.pass.value != "1234" && cont<3){
+	} else if (form.name.value!="doctor" || form.pass.value!="1234" && cont<3){
 		cont++;
 		alert("Error!"+cont+"de 3");
 	} while (cont==3){
