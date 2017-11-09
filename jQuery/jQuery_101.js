2a4a1c17ef6a53c69447600edcbf7f1e85a6437d
“puribey” <kenshipichu@gmail.com>
2017-11-09 15:11:47 -0300

    comienzo jQuery

---
+++ jQuery/jQuery_101.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$('.caption').click(function(){
+		$(this).toggleClass('negro');
+	});
+});
+
+
+//.addClass('nombreClase');
+//.removeClass('nombreClase');
+
+/* 
+	$('.display').click(function(){
+		$(this).hide(); // el contrario es .show(tiene dos parametros)
+	});
+
+	$('.caption').click(function(){
+		$(this).toggleClass('negro');
+	});
+*/
