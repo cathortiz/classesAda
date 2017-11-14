42a7403708750f8c1d9bd3ba05d065c750d05dfc
“puribey” <kenshipichu@gmail.com>
2017-11-13 23:18:58 -0300

    ejercicios

--- mentoring/pele/ejercicio.js
+++ mentoring/pele/ejercicio.js
@@ -21,8 +21,9 @@ var resultado= suma/notas.length;
 console.log(`El promedio de ${nombre} es: ${resultado}/10`);
 
 
+// ---------------------------------------------- 
 
-// Ejercicio 2
+Ejercicio 2
 Realizar un algoritmo para la conversiÛn de dÛlares a pesos,
 ingresando el monto a convertir y la cotizacion del dia
 
@@ -35,12 +36,12 @@ alert(`Su monto en pesos es de AR$ ${pesos}.-`);
 
 
 
+// ----------------------------------------------- 
+
 Ejercicio 3 
 realice el calculo del sueldo de un empleado ingresando el sueldo neto y sabiendo 
 que se le descuenta un 15 % por cargas sociales
 y se le suma un 10% si tuvo asistencia perfecta (preguntar si  tuvo ausencias)
-
-
 
 
 var sueldoNeto = parseFloat(prompt("Ingrese su sueldo neto:"));
@@ -56,6 +57,7 @@ if(asistencia == "si"){
 }
 
 
+// ------------------------------------------------ 
 
 Ejercicio 4
 
@@ -104,5 +106,141 @@ if (formaDePago == 1){
 
 
 /////////  B  //////////
+
+
+var opcion = prompt("Ingrese una opción del 1 al 5:");
+
+switch(opcion){
+	case "1":
+	alert("Su deuda es ... ");
+	break;
+	case "2":
+	alert("Será atendido en instantes");
+	break;
+	case "3":
+	alert("Todos los operadores se encuentran ocupados");
+	break;
+	case "4":
+	alert("Opción invalida");
+	break;
+	case "5":
+	alert("Gracias por usar nuestro servicio");
+	break;
+}
+
+
+
+/////////  C  //////////
+
+var cuotas = prompt("En cuántas cuotas desea pagar: 1, 12, 24 o 36 ?");
+var total = 1000;
+var cadaCuota = 0;
+
+switch(cuotas){
+	case "1":
+	alert(`Usted no posee recargo. Su saldo a pagar es de ${total.toFixed(2)}`);
+	break;
+	case "12":
+	cadaCuota = total/12;
+	total = ((cadaCuota*20)/100)+cadaCuota;
+	alert(`Usted no posee recargo. Su saldo a pagar es de ${total.toFixed(2)}`);
+	break;
+	case "24":
+	cadaCuota = total/24;
+	total = ((cadaCuota*45)/100)+cadaCuota;
+	alert(`Usted no posee recargo. Su saldo a pagar es de ${total.toFixed(2)}`);
+	break;
+	case "36":
+	cadaCuota = total/36;
+	total = ((cadaCuota*70)/100)+cadaCuota;
+	alert(`Usted no posee recargo. Su saldo a pagar es de ${total.toFixed(2)}`);
+	break;
+}
+
+
+// --------------------------------------------------- 
+
+Ejercicio 5
+pedirle a la persona un numero de 1 al 10
+y luego mostrar por pantalla los numeros siguientes hasta llegar a 10. 
+Ejemplo si escribibio 6, mostrar 7, 8, 9, 10.
+
+
+var numero = parseInt(prompt("Ingrese un numero del 1 al 10"));
+
+for (var i = numero; i <=10 ; i++){
+	console.log(i);
+}
+
+
+// --------------------------------------------------- 
+
+Ejercicio 6 
+Leé numeros ingresados por el usuario ingresando 0 para terminar.
+Emití, al finalizar, cuantos fueron mayores a 10, cu·ntos menores o iguales a 10.
+
+
+var arr = [];
+var num;
+
+while(num != 0 ){
+	num = parseInt(prompt("Ingresa un numero"));
+	arr.push(num);
+}
+
+console.log(`El array generado es: ${arr}`);
+
+var menores = 0;
+var mayores = 0;
+
+for (var i = 0 ; i < arr.length ; i ++){
+	if (arr[i]<=10){
+		menores++;
+	} else {
+		mayores++;
+	}
+}
+
+console.log(`Los numeros del array menores o iguales a 10 son: ${menores}`);
+console.log(`Los numeros del array mayores a 10 son: ${mayores}`);
+
+
+// --------------------------------------------------- 
+
+Ejercicio 7
+realizar un algoritmo que muestre la tabla de multiplicar del 1 al 10, 
+de un numero ingresado por el usuario ejemplo si ingreso 2 que muestre
+2 x 1 = 2
+2 x 2 = 4
+
+
+var num = parseInt(prompt("Ingresa un numero del 1 al 10"));
+var multiplicar = 0;
+
+for (var i = 1; i <=10; i++){
+	multiplicar = num * i;
+	console.log(`${num} x ${i} = ${multiplicar}`);
+}
+
+
+// --------------------------------------------------- 
+
+Ejercicio 8
+Se escriben el promedio y el nombre de 10 alumnos, al finalizar muestre el nombre 
+y promedio del mejor.
+
 */
+var nombres = ["Laura","Priscila","Luciana","Gia","Paola","Agustina","Belen","Camila"];
+var promedio = [7.5,6.5,8,8.5,9,8,9.5,6];
+var max =0;
+
+for(var i=0; i<promedio.length; i++){
+	
+}
+
+
+
+
+
+
 
