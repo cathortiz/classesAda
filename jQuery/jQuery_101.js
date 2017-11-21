e9511e9e5cab65392642f7d272581bbf28e50d1b
“puribey” <kenshipichu@gmail.com>
2017-11-21 12:29:06 -0300

    jquerry 1

--- jQuery/jQuery_101.js
+++ jQuery/jQuery_101.js
@@ -1,19 +1,44 @@
+
 $(document).ready(function(){
-	$('.caption').click(function(){
-		$(this).toggleClass('negro');
+	$('.btnEdit').on('click',function(){ // se usa una clase para poder reusar el html
+		var btnID =  $(this).prop('id'); // forma vieja = $(this).attr('id');
+		
+		// $(this).toggleClass('pink');
+		alert(`El id de este boton es: ${btnID}`);
+
 	});
 });
 
 
-//.addClass('nombreClase');
-//.removeClass('nombreClase');
+
 
 /* 
+	$(document).ready(function(){
+	
+	});
+
 	$('.display').click(function(){
 		$(this).hide(); // el contrario es .show(tiene dos parametros)
 	});
 
-	$('.caption').click(function(){
+	$('.caption').click(function(){ 
 		$(this).toggleClass('negro');
+	});
+	
+	//.addClass('nombreClase');
+	//.removeClass('nombreClase');
+
+	// .css({"color":"blue";});
+	// .prop("diplay":none);
+
+	$('#enviar').on('click', function(){ // = a doc.getElementByI(id).addEventListener('evento',function())
+		// funcion 
+	});
+
+	// Las funciones pueden estar hechas aparte y llamadas en la funcion de callback
+	
+	// $('#btn1,#btn2').on('click',function(){
+		$(this).toggleClass('pink');
+		alert("Estas seguro que quieres editar?");
 	});
 */
